264a05b1ec2337bc1894f782d9203dbb24d03fa6
155mila <58401380+155mila@users.noreply.github.com>
2022-06-17 10:45:17 +0300

    feat: add basic page layout

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ (() => {
+
+eval("/**import style from './scss/style.scss'*/\n\n/*import image from './images/lazy.png';\n\nconst createImage = (src) => new Promise((res, rej) => {\n  const img = new Image();\n  img.onload = () => res(img);\n  img.onerror = rej;\n  img.src = src;\n});\n\nasync function render() {\n  const subHeader = document.createElement('h2');\n  subHeader.innerHTML = 'This elements was created by js';\n  const myImage = await createImage(image);\n  document.body.appendChild(subHeader);\n  document.body.appendChild(myImage);\n}\n\nrender();*/\n\n\n//# sourceURL=webpack://museum/./index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./index.js"]();
+/******/ 	
+/******/ })()
+;
