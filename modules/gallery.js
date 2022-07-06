c04e7282eac30eee1cd46ef9543d5431734652dc
155mila <58401380+155mila@users.noreply.github.com>
2022-07-06 12:38:23 +0300

    feat: implement animation img gallery

--- modules/gallery.js
+++ modules/gallery.js
@@ -35,16 +35,53 @@ function gallery() {
   function createImg(arr) {
     console.log(arr);
     arr.map((item) => {
-      console.log(item);
+      //console.log(item);
       const img = document.createElement('img');
-      img.classList.add('gallery-img');
+      img.classList.add('gallery-img', 'anim-show');
       img.src = `assets/img/gallery/${item}.jpg`;
       img.alt = `foto picture`;
       pictureInnerContainer.append(img);
-      console.log(img);
+      //console.log(img);
     });
   }
-
   createImg(arrUrl);
+
+  const animItems = document.querySelectorAll('.anim-show');
+
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll(params) {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add('_active');
+      } else {
+        if (!animItem.classList.contains('gallery-img')) {
+          animItem.classList.remove('_active');
+        }
+      }
+    }
+
+    function offset(el) {
+      const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+    }
+  }
+
+  animOnScroll();
 }
 export default gallery;
